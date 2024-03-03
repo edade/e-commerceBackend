06edac6bbab277283cd0a44aa9c2b44d9ca4e9ae
Eda Kalaycioglu <edakalaycioglu@gmail.com>
2024-03-03 18:59:33 +0300

    DtoConvertion

--- src/main/java/com/workintech/ecommercebackend/util/DtoConvertion.java
+++ src/main/java/com/workintech/ecommercebackend/util/DtoConvertion.java
@@ -1,7 +1,9 @@
 package com.workintech.ecommercebackend.util;
 
 import com.workintech.ecommercebackend.dto.CategoryResponseDto;
+import com.workintech.ecommercebackend.dto.ProductResponseDto;
 import com.workintech.ecommercebackend.entity.Category;
+import com.workintech.ecommercebackend.entity.Product;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -17,8 +19,43 @@ public class DtoConvertion {
         }
         return categoryResponses;
     }
+
+    public static List<ProductResponseDto> convertProducts(List<Product> products){
+        List<ProductResponseDto> productResponses = new ArrayList<>();
+
+        for (Product product : products) {
+            Category category = product.getCategory();
+            CategoryResponseDto categoryResponseDto = null;
+            if (category != null) {
+                categoryResponseDto = new CategoryResponseDto(category.getTitle(), category.getGender());
+            }
+
+            ProductResponseDto productResponseDto = new ProductResponseDto(
+                    product.getName(),
+                    product.getDescription(),
+                    product.getPrice(),
+                    categoryResponseDto
+            );
+            productResponses.add(productResponseDto);
+        }
+        return productResponses;
+    }
     public static CategoryResponseDto convertCategory(Category category){
         return new CategoryResponseDto(category.getTitle(),category.getGender());
+    }
+    public static ProductResponseDto convertProduct(Product product){
+        Category category = product.getCategory();
+        CategoryResponseDto categoryResponseDto = null;
+        if (category != null) {
+            categoryResponseDto = new CategoryResponseDto(category.getTitle(), category.getGender());
+        }
+
+        return new ProductResponseDto(
+                product.getName(),
+                product.getDescription(),
+                product.getPrice(),
+                categoryResponseDto
+        );
     }
 
 }
