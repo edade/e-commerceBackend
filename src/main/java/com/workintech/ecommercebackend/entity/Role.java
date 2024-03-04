581a9300f63cd8a16ce8b4ca9e89e17d55749158
Eda Kalaycioglu <edakalaycioglu@gmail.com>
2024-03-05 00:06:11 +0300

    role user entity

---
+++ src/main/java/com/workintech/ecommercebackend/entity/Role.java
@@ -0,0 +1,38 @@
+package com.workintech.ecommercebackend.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Entity
+@Table(name= "role", schema = "ecommerce")
+public class Role implements GrantedAuthority {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+    @Column(name = "authority")
+    private String authority;
+
+    @OneToMany(mappedBy = "role", cascade = CascadeType.ALL)
+    private List<User> userList;
+
+    public void addUser(User user){
+        if(userList == null){
+            userList = new ArrayList<>();
+        }
+        userList.add(user);}
+
+    @Override
+    public String getAuthority() {
+        return authority;
+    }
+}
